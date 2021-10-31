28bcf415b7e75a9c73b478dae657546d97d812d4
Shubh9907 <shubhamv60047@gmail.com>
2021-10-31 19:51:10 +0530

    Added code to find the greatest of three Intergers

--- Main.java
+++ Main.java
@@ -1,7 +1,22 @@
 package com.bridgelabz;
 
 public class Main {
+    /*
+    Method to find the greatest of three Integers
+     */
+    public static void findMax(int num1,int num2,int num3){
+        int max=num1;
+        if (max<num2){
+            max=num2;
+        }
+        if (max<num3){
+            max=num3;
+        }
+        System.out.println("The Greatest Number is:- " +max);
+    }
     public static void main(String[] args) {
         System.out.println("Welcome to Test Maximum Program");
+        int num1=196,num2=156,num3=659;
+        findMax(num1,num2,num3);
     }
 }
